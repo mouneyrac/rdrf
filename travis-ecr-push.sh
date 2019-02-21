54b77a442af63de21b4e1d72f9e2a8dbde6825ab
lee <id2359@yahoo.com>
2019-02-21 17:26:17 +0800

    ops #1678 allow ecr images of any branch

--- travis-ecr-push.sh
+++ travis-ecr-push.sh
@@ -6,10 +6,10 @@ set -e
 # Push built image to ecr staging
 #
 
-if [ x"$BRANCH_NAME" != x"master" -a x"$BRANCH_NAME" != x"next_release" -a x"$BRANCH_NAME" != x"staging" ]; then
-    echo "Branch $BRANCH_NAME is not deployable. Skipping prod build and tests"
-    exit 0
-fi
+#if [ x"$BRANCH_NAME" != x"master" -a x"$BRANCH_NAME" != x"next_release" -a x"$BRANCH_NAME" != x"staging" ]; then
+#    echo "Branch $BRANCH_NAME is not deployable. Skipping prod build and tests"
+#    exit 0
+#fi
 
 pip install --user awscli # install aws cli w/o sudo
 export PATH=$PATH:$HOME/.local/bin # put aws in the path
