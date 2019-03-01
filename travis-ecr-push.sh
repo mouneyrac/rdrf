dff1860c6463a53578535c9def0ec2709939e578
lee <id2359@yahoo.com>
2019-03-01 15:32:41 +0800

    Added build ver to build correct tag

--- travis-ecr-push.sh
+++ travis-ecr-push.sh
@@ -15,9 +15,10 @@ pip install --user awscli # install aws cli w/o sudo
 export PATH=$PATH:$HOME/.local/bin # put aws in the path
 
 eval $(aws ecr get-login --no-include-email --region ap-southeast-2) #needs AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY envvars
-env
 
-tag="?????"
+build_ver=$(./develop.sh build-version)
+
+tag="muccg/$CCG_PROJECT:$build_ver"
 
 ecrtag="$AWSACCOUNTID.dkr.ecr.us-east-1.amazonaws.com/$tag"  # needs account id in env
 
