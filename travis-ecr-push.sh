f1ce7cadee5af3763b64ee029c50ee24ddd27126
lee <id2359@yahoo.com>
2019-03-05 14:46:14 +0800

    corrected region to ap-southeast-2 in ecr repo

--- travis-ecr-push.sh
+++ travis-ecr-push.sh
@@ -14,16 +14,15 @@ set -e
 pip install --user awscli # install aws cli w/o sudo
 export PATH=$PATH:$HOME/.local/bin # put aws in the path
 
-eval $(aws ecr get-login --no-include-email --region ap-southeast-2 | sed 's|https://||') #needs AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY envvars
+eval $(aws ecr get-login --no-include-email --region ap-southeast-2) # needs AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY envvars
 
 build_ver=$(./develop.sh build-version)
+
 image="muccg/rdrf:$build_ver"
 image=`echo $image | sed -e 's/\\r//g'`
 echo "image = $image"
-docker images
 
-ecrtag="$AWSACCOUNTID.dkr.ecr.us-east-1.amazonaws.com/$image"  # needs account id in env
-
+ecrtag="$AWSACCOUNTID.dkr.ecr.ap-southeast-2.amazonaws.com/$image"  # needs account id in env
 ecrtag=`echo $ecrtag | sed -e 's/\\r//g'`
 
 docker tag $image $ecrtag
