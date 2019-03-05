439bb6c7d4ca70d13c4b0e46dfcea4f10c15d732
lee <id2359@yahoo.com>
2019-03-05 15:54:20 +0800

    rdrf #794 ecr integration for deployable branches

--- travis-prodbuild.sh
+++ travis-prodbuild.sh
@@ -6,10 +6,10 @@ set -e
 # Production (deployable) build and tests
 #
 
-#if [ x"$BRANCH_NAME" != x"master" -a x"$BRANCH_NAME" != x"next_release" -a x"$BRANCH_NAME" != x"staging" ]; then
-#    echo "Branch $BRANCH_NAME is not deployable. Skipping prod build and tests"
-#    exit 0
-#fi
+if [ x"$BRANCH_NAME" != x"master" -a x"$BRANCH_NAME" != x"next_release" -a x"$BRANCH_NAME" != x"staging" ]; then
+    echo "Branch $BRANCH_NAME is not deployable. Skipping prod build and tests"
+    exit 0
+fi
 
 ./develop.sh run-builder checkout
 ./develop.sh run build node
