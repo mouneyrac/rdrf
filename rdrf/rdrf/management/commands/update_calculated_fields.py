42b85ad01b024bf3cb99a49c6b1777000a69903c
Jerome Mouneyrac <jerome@mouneyrac.com>
2019-05-24 13:08:27 +0800

    tmp work 5

--- rdrf/rdrf/management/commands/update_calculated_fields.py
+++ rdrf/rdrf/management/commands/update_calculated_fields.py
@@ -3,7 +3,7 @@ import json
 import requests
 from django.core.management.base import BaseCommand
 
-from rdrf.models.definition.models import CommonDataElement, RegistryForm, Section
+from rdrf.models.definition.models import CommonDataElement, RegistryForm, Section, RDRFContext, ContextFormGroupItem
 from registry.patients.models import Patient
 import urllib.parse
 
@@ -31,8 +31,8 @@ class Command(BaseCommand):
         start = time.time()
         # retrieve all cde with a calculated field
         cdes = CommonDataElement.objects.exclude(calculation='')
-        # for cde in cdes:
-        #     print(f"Code: {cde.code}")
+        for cde in cdes:
+            print(f"Code: {cde.code}")
 
         # Building the tree of form values so we can access them quickly.
         # for all registry
@@ -45,6 +45,12 @@ class Command(BaseCommand):
         datacdes = {}
 
         for form in forms:
+            # acontexts = RDRFContext.objects.filter(registry=form.registry)
+            # for acontext in acontexts:
+            #     # for context_form_group in acontext.context_form_group:
+            #     if acontext.context_form_group is not None:
+            #         print(f"Form: {form.name} Context id: {acontext.id} Context form name: {acontext.context_form_group.name}")
+
 
             sections = Section.objects.filter(code__in=form.get_sections())
 
@@ -90,6 +96,8 @@ class Command(BaseCommand):
 
         # print(f"{cde_values_dict}")
 
+        print("----------------------- BEGIN ALL CALCULATIONS --------------------------")
+
         # for registry in cde_values_dict:
         #     print(registry)
         # patients = PatientManager.get_by_registry(registry)
@@ -100,64 +108,86 @@ class Command(BaseCommand):
         # for all patient
         patients = Patient.objects.all()
         for patient in patients:
+
             # for all patient registry
             for registry in patient.rdrf_registry.all():
                 # if registry is in the cde_values_dict
                 if registry.code in cde_values_dict:
                     for form in cde_values_dict[registry.code]:
                         jscontextvar = {}
+                        form_model = RegistryForm.objects.get(name=form,registry=registry)
+                        formgroupitems = ContextFormGroupItem.objects.filter(registry_form=form_model)
+                        contexts = RDRFContext.objects.filter(registry=registry,
+                                                              object_id=patient.id,
+                                                              context_form_group__in=[formgroupitem.context_form_group for formgroupitem in formgroupitems])
 
-                        for section in cde_values_dict[registry.code][form]:
+                        context_ids = [context.id for context in contexts]
+                        # if no context, it means we are in the case of a single context like DD registry
+                        if not context_ids:
+                            context_ids = [c.id for c in patient.context_models]
 
-                            for cdecode in cde_values_dict[registry.code][form][section]:
-                                try:
-                                    cdevalue = patient.get_form_value(registry_code=registry.code,
-                                                                      data_element_code=cdecode, form_name=form,
-                                                                      section_code=section)
-                                    # if cde is a date then display the JS format.
-                                    # print(f"CDE TYPE: {cde_values_dict[registry.code][form][section][cdecode].datatype}")
-                                    if cde_values_dict[registry.code][form][section][cdecode].datatype == 'date':
-                                        cdevalue = datetime.strptime(cdevalue, '%Y-%m-%d').__format__("%d-%m-%Y")
-                                        # print(f"DATE: {cdevalue}")
-                                    jscontextvar[cdecode] = cdevalue
-                                except KeyError:
-                                    print(f"IGNORING EMPTY CDE VALUE {[c.id for c in patient.context_models]} | {registry.code} | {form} | {section} | {cdecode}")
 
-                        # for each calculayed cdes in this form, do a WS call
-                        for calculatedcde in cdes:
-                            if calculatedcde.code in jscontextvar.keys():
+                        print(f"Form: {form} contexts: {context_ids}")
+                        print("---------------------------------------")
 
-                                print(f"{calculatedcde.code}")
-                                # print(f"CALCULATION: {calculatedcde.calculation}")
-                                # print(f"PATIENT: {patient}")
-                                # print(f"PATIENT DATE OF BIRTH: {patient.date_of_birth.__format__('%Y-%m-%d')}")
-                                simple_patient = {'sex': patient.sex, 'date_of_birth': patient.date_of_birth.__format__("%Y-%m-%d")}
-                                # print(f"CONTEXT: {jscontextvar}")
-                                mockcode = """
-                                class Rdrf {
-                                    log(msg) {
-                                        console.log(msg);
+                        # for each context
+
+                        for context_id in context_ids:
+
+                            for section in cde_values_dict[registry.code][form]:
+
+                                for cdecode in cde_values_dict[registry.code][form][section]:
+
+
+                                    try:
+                                        cdevalue = patient.get_form_value(registry_code=registry.code,
+                                                                          data_element_code=cdecode, form_name=form,
+                                                                          section_code=section, context_id=context_id)
+                                        # if cde is a date then display the JS format.
+                                        # print(f"CDE TYPE: {cde_values_dict[registry.code][form][section][cdecode].datatype}")
+                                        if cdevalue is not None and cde_values_dict[registry.code][form][section][cdecode].datatype == 'date':
+                                            cdevalue = datetime.strptime(cdevalue, '%Y-%m-%d').__format__("%d-%m-%Y")
+                                            # print(f"DATE: {cdevalue}")
+                                        jscontextvar[cdecode] = cdevalue
+                                    except KeyError:
+                                        # mainly we ignore empty values.
+                                        print(f"KeyError for CDE value:{patient.combined_name} {[c.id for c in patient.context_models]} | {registry.code} | {form} | {section} | {cdecode}")
+
+                            # for each calculayed cdes in this form, do a WS call
+                            for calculatedcde in cdes:
+                                if calculatedcde.code in jscontextvar.keys():
+
+                                    print(f"{calculatedcde.code}")
+                                    # print(f"CALCULATION: {calculatedcde.calculation}")
+                                    # print(f"PATIENT: {patient}")
+                                    # print(f"PATIENT DATE OF BIRTH: {patient.date_of_birth.__format__('%Y-%m-%d')}")
+                                    simple_patient = {'sex': patient.sex, 'date_of_birth': patient.date_of_birth.__format__("%Y-%m-%d")}
+                                    # print(f"CONTEXT: {jscontextvar}")
+                                    mockcode = """
+                                    class Rdrf {
+                                        log(msg) {
+                                            console.log(msg);
+                                        }
+                                    
+                                        get(data, key) {
+                                            return data[key];
+                                            // console.log("A function is calling RDRF.get ADSAFE - ignoring...");
+                                        }
                                     }
-                                
-                                    get(data, key) {
-                                        return data[key];
-                                        // console.log("A function is calling RDRF.get ADSAFE - ignoring...");
-                                    }
-                                }
-                                
-                                RDRF = new Rdrf();"""
-                                context_js_code = f"{mockcode} patient = {json.dumps(simple_patient)}; context = {json.dumps(jscontextvar)}; {calculatedcde.calculation}"
-                                headers = {'Content-Type': 'application/json'}
+                                    
+                                    RDRF = new Rdrf();"""
+                                    context_js_code = f"{mockcode} patient = {json.dumps(simple_patient)}; context = {json.dumps(jscontextvar)}; {calculatedcde.calculation}"
+                                    headers = {'Content-Type': 'application/json'}
 
-                                # jscode = {
-                                #     "jscode": "const%20context%20%3D%20%7BmndCuttingFood%3A1%2C%0AmndCuttingFoodWithGastrodtomy%3A1%2C%0AmndSpeech%3A1%2C%0AmndSalivation%3A1%2C%0AmndSwallowing%3A1%2C%0AmndHandwriting%3A1%2C%0AmndDressingHygiene%3A1%2C%0AmndBed%3A1%2C%0AmndWalking%3A1%2C%0AmndClimbingStairs%3A1%2C%0AmndDyspnea%3A1%2C%0AmndOrthopnea%3A1%2C%0AmndRespiratoryInsufficiency%3A1%2C%0A%7D%3B%0A%0Afunction%20bad%28value%29%20%7B%0A%20%20%20return%20%20%28value%20%3D%3D%3D%20null%29%20%7C%7C%20%28%20value%20%3D%3D%3D%20undefined%29%20%7C%7C%20isNaN%28value%29%3B%0A%7D%0A%0Afunction%20getFloat%28x%29%20%7B%0A%20%20%20var%20y%20%3D%20parseFloat%28x%29%3B%0A%20%20%20if%20%28%21%20isNaN%28y%29%29%20%7B%0A%20%20%20%20%20%20%20return%20y%3B%0A%20%20%20%7D%0A%20%20%20return%20null%3B%0A%7D%0A%0Afunction%20getFilledOutScore%28x%2Cy%29%20%7B%0A%20%20%20var%20xval%20%3D%20getFloat%28x%29%3B%0A%20%20%20if%20%28xval%20%21%3D%3D%20null%29%20%7B%0A%20%20%20%20%20%20return%20xval%3B%0A%20%20%20%7D%0A%20%20%20return%20getFloat%28y%29%3B%0A%7D%0A%0Afunction%20getOptionE%28context%29%20%7B%0A%20var%20cutting%20%3D%20%20parseInt%28context.mndCuttingFood%2C%2010%29%3B%0A%20var%20gastrostomy%20%3D%20%20parseInt%28context.mndCuttingFoodWithGastrodtomy%2C%2010%29%3B%0A%20return%20getFilledOutScore%28cutting%2C%20gastrostomy%29%3B%0A%7D%0A%0Avar%20speech%3D%20parseInt%28context.mndSpeech%2C%2010%29%3B%0Avar%20salivation%20%3D%20parseInt%28context.mndSalivation%2C%2010%29%3B%0Avar%20swallowing%20%3D%20%20parseInt%28context.mndSwallowing%2C%2010%29%3B%0Avar%20handwriting%20%3D%20%20parseInt%28context.mndHandwriting%2C%2010%29%3B%0Avar%20hygiene%20%3D%20%20parseInt%28context.mndDressingHygiene%2C%2010%29%3B%0Avar%20bed%20%3D%20%20parseInt%28context.mndBed%2C%2010%29%3B%0Avar%20walking%20%3D%20%20parseInt%28context.mndWalking%2C%2010%29%3B%0Avar%20climbing%20%3D%20%20parseInt%28context.mndClimbingStairs%2C%2010%29%3B%0Avar%20dyspnea%20%3D%20%20parseInt%28context.mndDyspnea%2C%2010%29%3B%0Avar%20orthopnea%20%3D%20%20parseInt%28context.mndOrthopnea%2C%2010%29%3B%0Avar%20respiratory%20%3D%20parseInt%28context.mndRespiratoryInsufficiency%2C%2010%29%3B%0A%0Avar%20cutting_filled%20%3D%20getOptionE%28context%29%3B%0A%0Avar%20total%20%3D%20%28speech%20%2B%20salivation%20%2B%20swallowing%20%2B%20handwriting%20%2B%20cutting_filled%20%2B%20hygiene%20%2B%20bed%20%2B%20walking%20%2B%20climbing%20%2B%20dyspnea%20%2B%20orthopnea%20%2B%20respiratory%29%3B%0A%0Acontext.result%20%3D%20total%3B"}
-                                jscode = {"jscode": urllib.parse.quote(context_js_code)}
-                                # print(jscode)
+                                    # jscode = {
+                                    #     "jscode": "const%20context%20%3D%20%7BmndCuttingFood%3A1%2C%0AmndCuttingFoodWithGastrodtomy%3A1%2C%0AmndSpeech%3A1%2C%0AmndSalivation%3A1%2C%0AmndSwallowing%3A1%2C%0AmndHandwriting%3A1%2C%0AmndDressingHygiene%3A1%2C%0AmndBed%3A1%2C%0AmndWalking%3A1%2C%0AmndClimbingStairs%3A1%2C%0AmndDyspnea%3A1%2C%0AmndOrthopnea%3A1%2C%0AmndRespiratoryInsufficiency%3A1%2C%0A%7D%3B%0A%0Afunction%20bad%28value%29%20%7B%0A%20%20%20return%20%20%28value%20%3D%3D%3D%20null%29%20%7C%7C%20%28%20value%20%3D%3D%3D%20undefined%29%20%7C%7C%20isNaN%28value%29%3B%0A%7D%0A%0Afunction%20getFloat%28x%29%20%7B%0A%20%20%20var%20y%20%3D%20parseFloat%28x%29%3B%0A%20%20%20if%20%28%21%20isNaN%28y%29%29%20%7B%0A%20%20%20%20%20%20%20return%20y%3B%0A%20%20%20%7D%0A%20%20%20return%20null%3B%0A%7D%0A%0Afunction%20getFilledOutScore%28x%2Cy%29%20%7B%0A%20%20%20var%20xval%20%3D%20getFloat%28x%29%3B%0A%20%20%20if%20%28xval%20%21%3D%3D%20null%29%20%7B%0A%20%20%20%20%20%20return%20xval%3B%0A%20%20%20%7D%0A%20%20%20return%20getFloat%28y%29%3B%0A%7D%0A%0Afunction%20getOptionE%28context%29%20%7B%0A%20var%20cutting%20%3D%20%20parseInt%28context.mndCuttingFood%2C%2010%29%3B%0A%20var%20gastrostomy%20%3D%20%20parseInt%28context.mndCuttingFoodWithGastrodtomy%2C%2010%29%3B%0A%20return%20getFilledOutScore%28cutting%2C%20gastrostomy%29%3B%0A%7D%0A%0Avar%20speech%3D%20parseInt%28context.mndSpeech%2C%2010%29%3B%0Avar%20salivation%20%3D%20parseInt%28context.mndSalivation%2C%2010%29%3B%0Avar%20swallowing%20%3D%20%20parseInt%28context.mndSwallowing%2C%2010%29%3B%0Avar%20handwriting%20%3D%20%20parseInt%28context.mndHandwriting%2C%2010%29%3B%0Avar%20hygiene%20%3D%20%20parseInt%28context.mndDressingHygiene%2C%2010%29%3B%0Avar%20bed%20%3D%20%20parseInt%28context.mndBed%2C%2010%29%3B%0Avar%20walking%20%3D%20%20parseInt%28context.mndWalking%2C%2010%29%3B%0Avar%20climbing%20%3D%20%20parseInt%28context.mndClimbingStairs%2C%2010%29%3B%0Avar%20dyspnea%20%3D%20%20parseInt%28context.mndDyspnea%2C%2010%29%3B%0Avar%20orthopnea%20%3D%20%20parseInt%28context.mndOrthopnea%2C%2010%29%3B%0Avar%20respiratory%20%3D%20parseInt%28context.mndRespiratoryInsufficiency%2C%2010%29%3B%0A%0Avar%20cutting_filled%20%3D%20getOptionE%28context%29%3B%0A%0Avar%20total%20%3D%20%28speech%20%2B%20salivation%20%2B%20swallowing%20%2B%20handwriting%20%2B%20cutting_filled%20%2B%20hygiene%20%2B%20bed%20%2B%20walking%20%2B%20climbing%20%2B%20dyspnea%20%2B%20orthopnea%20%2B%20respiratory%29%3B%0A%0Acontext.result%20%3D%20total%3B"}
+                                    jscode = {"jscode": urllib.parse.quote(context_js_code)}
+                                    # print(jscode)
 
-                                resp = requests.post(url='http://node_js_evaluator:3131/eval', headers=headers,
-                                                     json=jscode)
-                                print('Result: {}'.format(resp.json()))
-                                print(f"----------------------- END CALCULATION --------------------------")
+                                    resp = requests.post(url='http://node_js_evaluator:3131/eval', headers=headers,
+                                                         json=jscode)
+                                    print('Result: {}'.format(resp.json()))
+                                    print(f"----------------------- END CALCULATION --------------------------")
 
                         # store the new form value in the ClinicalData model
 
@@ -166,6 +196,10 @@ class Command(BaseCommand):
 
         # Run some test
         # set data
+
+        # Code: DDAgeAtDiagnosis
+        #   DateOfDiagnosis
+
         # Code: CDE00024
         #   CDE00013 LDLCholesterolAdjTreatment CDE00004 CDE00003 FHFamHistTendonXanthoma FHFamHistArcusCornealis CDEfhDutchLipidClinicNetwork DateOfAssessment
         #   CDEIndexOrRelative
